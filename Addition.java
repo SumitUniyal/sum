b228a46be2c712c2c81a0fc4d9a6d39af147a8c8
Sumit Uniyal <sumit.uniyal03@gmail.com>
2020-11-27 12:13:55 +0530

    Addition of three numbers
    adding numbers

--- Addition.java
+++ Addition.java
@@ -4,6 +4,7 @@ class Addition
   {
     int a=5;
     int b=7;
-    System.out.println("Addition of two numbers is : "+(a+b));
+    int c=10;
+    System.out.println("Addition of two numbers is : "+(a+b+c));
   }
 }
